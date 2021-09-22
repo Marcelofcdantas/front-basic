b010b210a014b1a6b3f5f028735345e9226e3f82
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-09-22 18:50:38 -0300

    funcao principal javascript iniciada

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Código que será executado quando o navegador carregar
+const imageArea = document.querySelector('.imageArea');
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const link = 'https://us-central1-squid-apis.cloudfunctions.net/test-front-basic';
+  try {
+    const datas =  await fetch(link);
+    const jsonData = await datas.json();
+    const loader = document.querySelector('.loader');
+    const loading = document.querySelector('.loading');
+    loading.remove();
+    loader.remove();
+    console.log(jsonData);
+    jsonData.forEach((photo) => {
+      const images = {
+        image: photo.imagens.thumbnail.url,
+        link: photo.link,
+      };
+      images.onclick = function() {
+        window.location.href = photo.link;
+    };
+      const item = settingImages(images);
+      imageArea.appendChild(item);
+    });
+    setTarget();
+  } 
+  catch {
+
+  }
 });
